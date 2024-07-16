54cf81809fe757c771868cf138c18e6a574b104d
Jonathan <nath.joe34@gmail.com>
2024-07-16 16:29:34 +0700

    UPDATE margin bottom deskripsi founder

--- client-side/src/pages/guest/About.jsx
+++ client-side/src/pages/guest/About.jsx
@@ -109,7 +109,7 @@ const About = () =>{
                             <div className="flex flex-col mr-14 w-[80%]">
                                 <h3 className="text-xl font-bold text-[#4B241A]">John Doe</h3>
                                 <h4 className="mt-1 text-lg font-quicksand font-medium text-[24px]">Founder</h4>
-                                <p className="font-quicksand font-medium text-[20px] mr-10 text-justify">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                                <p className="font-quicksand font-medium text-[20px] mr-10 text-justify mb-10">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
                             </div>
                         </div>
                     </div>
