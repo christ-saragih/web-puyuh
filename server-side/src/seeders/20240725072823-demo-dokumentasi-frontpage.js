ddf630e82347207553bb1381d431e3c1f9a9d525
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-07-25 15:33:55 +0700

    CREATE : Data Seeder Dokumentasi Frontpage

---
+++ server-side/src/seeders/20240725072823-demo-dokumentasi-frontpage.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const { faker } = require("@faker-js/faker");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+
+        const dokumentasi = [];
+
+        for (let i = 0; i < 3; i++) {
+            const data = {
+                nama: faker.lorem.word(),
+                image: faker.image.url(),
+            };
+            dokumentasi.push(data);
+        }
+
+        await queryInterface.bulkInsert(
+            "DokumentasiFrontpages",
+            dokumentasi,
+            {}
+        );
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete("DokumentasiFrontpages", null, {});
+    },
+};
