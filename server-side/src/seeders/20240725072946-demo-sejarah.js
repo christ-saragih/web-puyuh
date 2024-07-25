9d284b61e5455dc5a81232d8a21d1760634f9c5d
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-07-25 15:34:13 +0700

    CREATE : Data Seeder Sejarah

---
+++ server-side/src/seeders/20240725072946-demo-sejarah.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { faker } = require("@faker-js/faker");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+        await queryInterface.bulkInsert(
+            "Sejarahs",
+            [
+                {
+                    judul: faker.lorem.sentence(),
+                    deskripsi: faker.lorem.paragraphs(),
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ],
+            {}
+        );
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete("Sejarahs", null, {});
+    },
+};
