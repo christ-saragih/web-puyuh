7091d6a1eed4c69a8fd4c60492700bca3fc461d8
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-08-01 09:42:49 +0700

    UPDATE : Mengubah variable adminBiodataController menjadi adminController

--- server-side/src/routes/adminRoutes.js
+++ server-side/src/routes/adminRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const adminBiodataController = require("../controllers/adminController");
+const adminController = require("../controllers/adminController");
 
 const {
     authenticateAdminToken,
 } = require("../middleware/authenticateAdminToken");
 const authorizeRole = require("../middleware/authorizeRole");
 
-router.get("/", adminBiodataController.findAll);
-router.get("/:id", adminBiodataController.findOne);
+router.get("/", adminController.findAll);
+router.get("/:id", adminController.findOne);
 
 module.exports = router;
