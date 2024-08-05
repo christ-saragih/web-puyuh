de6a02e9cb18f441f92dc165a11ab24d96002b7e
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-08-05 13:48:23 +0700

    UPDATE : Menambahkan fungsi Upsert

--- server-side/src/controllers/kontakFrontpageController.js
+++ server-side/src/controllers/kontakFrontpageController.js
@@ -1,22 +1,37 @@
 const { KontakFrontpage } = require("../models");
-const fs = require("fs");
-const path = require("path");
 
-// Create
-exports.create = async (req, res) => {
+// Upsert
+exports.upsert = async (req, res) => {
     try {
-        const { url_map, alamat, email, no_phone } = req.body;
-        const kontak = await KontakFrontpage.create({
-            url_map,
-            alamat,
-            email,
-            no_phone,
-        });
+        const kontak = await KontakFrontpage.findOne();
+        if (!kontak) {
+            const { url_map, alamat, email, no_phone } = req.body;
+            const kontak = await KontakFrontpage.create({
+                url_map,
+                alamat,
+                email,
+                no_phone,
+            });
 
-        res.status(201).json({
-            message: "Kontak Berhasil Ditambahkan!",
-            data: kontak,
-        });
+            res.status(201).json({
+                message: "Kontak Berhasil Ditambahkan!",
+                data: kontak,
+            });
+        } else {
+            const { url_map, alamat, email, no_phone } = req.body;
+
+            await kontak.update({
+                url_map,
+                alamat,
+                email,
+                no_phone,
+            });
+
+            res.status(200).json({
+                message: "Kontak berhasil diperbaharui",
+                data: kontak,
+            });
+        }
     } catch (error) {
         if (error.name === "SequelizeValidationError") {
             const messages = error.errors.map((err) => err.message);
@@ -32,6 +47,37 @@ exports.create = async (req, res) => {
         }
     }
 };
+
+// // Create
+// exports.create = async (req, res) => {
+//     try {
+//         const { url_map, alamat, email, no_phone } = req.body;
+//         const kontak = await KontakFrontpage.create({
+//             url_map,
+//             alamat,
+//             email,
+//             no_phone,
+//         });
+
+//         res.status(201).json({
+//             message: "Kontak Berhasil Ditambahkan!",
+//             data: kontak,
+//         });
+//     } catch (error) {
+//         if (error.name === "SequelizeValidationError") {
+//             const messages = error.errors.map((err) => err.message);
+//             res.status(400).json({
+//                 message: "Validation error",
+//                 errors: messages,
+//             });
+//         } else {
+//             res.status(500).json({
+//                 message: "Internal server error",
+//                 error: error.message,
+//             });
+//         }
+//     }
+// };
 
 // Read All
 exports.findAll = async (req, res) => {
@@ -68,37 +114,37 @@ exports.findOne = async (req, res) => {
     }
 };
 
-// Update
-exports.update = async (req, res) => {
-    try {
-        const { url_map, alamat, email, no_phone } = req.body;
+// // Update
+// exports.update = async (req, res) => {
+//     try {
+//         const { url_map, alamat, email, no_phone } = req.body;
 
-        const kontak = await KontakFrontpage.findByPk(req.params.id);
-        if (!kontak) {
-            return res.status(404).json({ message: "Kontak tidak ada!" });
-        }
+//         const kontak = await KontakFrontpage.findByPk(req.params.id);
+//         if (!kontak) {
+//             return res.status(404).json({ message: "Kontak tidak ada!" });
+//         }
 
-        await kontak.update(req.body);
+//         await kontak.update(req.body);
 
-        res.status(200).json({
-            message: "Kontak berhasil diperbaharui",
-            data: kontak,
-        });
-    } catch (error) {
-        if (error.name === "SequelizeValidationError") {
-            const messages = error.errors.map((err) => err.message);
-            res.status(400).json({
-                message: "Validation error",
-                errors: messages,
-            });
-        } else {
-            res.status(500).json({
-                message: "Internal server error",
-                error: error.message,
-            });
-        }
-    }
-};
+//         res.status(200).json({
+//             message: "Kontak berhasil diperbaharui",
+//             data: kontak,
+//         });
+//     } catch (error) {
+//         if (error.name === "SequelizeValidationError") {
+//             const messages = error.errors.map((err) => err.message);
+//             res.status(400).json({
+//                 message: "Validation error",
+//                 errors: messages,
+//             });
+//         } else {
+//             res.status(500).json({
+//                 message: "Internal server error",
+//                 error: error.message,
+//             });
+//         }
+//     }
+// };
 
 // Delete
 exports.delete = async (req, res) => {
