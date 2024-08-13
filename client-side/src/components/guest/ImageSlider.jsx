8ba0276f6ff5b91c964693cde8d98e891678893e
Jonathan <nath.joe34@gmail.com>
2024-08-13 15:00:08 +0700

    FETCH API dokumentasi for ImageSlider

--- client-side/src/components/guest/ImageSlider.jsx
+++ client-side/src/components/guest/ImageSlider.jsx
@@ -1,5 +1,7 @@
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination, Navigation, Autoplay } from 'swiper/modules';
+import { getDocumentation } from '../../services/documentation.service'; // Import layanan yang sudah Anda buat
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -9,55 +11,49 @@ import 'swiper/css/autoplay';
 
 import '../../assets/style/index.css';
 
-import slide_image_1 from '../../assets/images/quail.jpg';
-import slide_image_2 from '../../assets/images/quail cage.jpg';
-import slide_image_3 from '../../assets/images/quail egg.jpg';
-import slide_image_4 from '../../assets/images/example-1.jpg';
+const ImageSlider = ({ className = "" }) => {
+  const [datas, setDatas] = useState([]);
 
-// Swiper.use([EffectCoverflow, Pagination, Navigation]);
+  useEffect(() => {
+    getDocumentation((data) => {
+      console.log(data);
+      setDatas(data);
+    });
+  }, []);
 
-const ImageSlider = (props) => {
-  const { className = "" } = props;
-    return (
-      <div className={ "container " +  className}>
-        <Swiper
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          loop={true}
-          slidesPerView={'auto'}
-          autoplay={{
-            delay: 5000,}
-          }
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2.5,
-          }}
-          pagination={{ el: '.swiper-pagination', clickable: true }}
-          modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
-          className="swiper_container"
-        >
-          <SwiperSlide>
-            <img src={slide_image_1} alt="slide_image" className='slide-image'  />
+  return (
+    <div className={"container " + className}>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        autoplay={{
+          delay: 5000,
+        }}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+        className="swiper_container"
+      >
+        {datas.map((data, index) => (
+          <SwiperSlide key={index}>
+            <img src={`http://localhost:3000/api/dokumentasi-frontpage/image/${data.image}`} alt={data.nama} className='slide-image' />
           </SwiperSlide>
-          <SwiperSlide>
-            <img src={slide_image_2} alt="slide_image" className='slide-image' />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={slide_image_3} alt="slide_image" className='slide-image' />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={slide_image_4} alt="slide_image" className='slide-image' />
-          </SwiperSlide>
-  
-          <div className="slider-controler">
-            <div className="swiper-pagination"></div>
-          </div>
-        </Swiper>
-      </div>
-    );
-  }
+        ))}
+        <div className="slider-controler">
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+    </div>
+    
+  );
+};
 
 export default ImageSlider;
