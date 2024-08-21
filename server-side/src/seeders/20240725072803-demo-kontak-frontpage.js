bcb4f2d9562085061ec31795533edaf56e3e4f38
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-08-21 12:12:19 +0700

    UPDATE : data seeder kontak frontpage untuk url_map

--- server-side/src/seeders/20240725072803-demo-kontak-frontpage.js
+++ server-side/src/seeders/20240725072803-demo-kontak-frontpage.js
@@ -19,7 +19,7 @@ module.exports = {
             [
                 {
                     url_map:
-                        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.4160554831756!2d106.7878496743095!3d-6.960148868143952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e683a7466b95bfd%3A0xfa59aaabf74caf24!2sP4S!5e0!3m2!1sid!2sid!4v1721894635275!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade',
+                        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.7116708630556!2d106.76757977430363!3d-6.558035064107707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c5dce5b6ebd3%3A0x7ba6ffb5d199befe!2sSekolah%20Tinggi%20Pariwisata%20Bogor!5e0!3m2!1sid!2sid!4v1724215276273!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
                     alamat: "2QQR+W5M, Cikembar, Sukabumi Regency, West Java 43157",
                     email: faker.internet.email({
                         firstName: "cvslamet",
