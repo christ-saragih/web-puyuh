09db367b0134eb371a4af6c89d57a10da558af1b
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-08-26 14:57:08 +0700

    UPDATE : model relation as investorBiodata

--- server-side/src/models/investor.js
+++ server-side/src/models/investor.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
             // define association here\
             Investor.hasOne(models.InvestorBiodata, {
                 foreignKey: "investorId",
+                as: "investorBiodata",
             });
             Investor.hasOne(models.InvestorAlamat, {
                 foreignKey: "investorId",
--- server-side/src/models/investorbiodata.js
+++ server-side/src/models/investorbiodata.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
             InvestorBiodata.belongsTo(models.Investor, {
                 foreignKey: "investorId",
+                as: "investor",
             });
         }
     }
