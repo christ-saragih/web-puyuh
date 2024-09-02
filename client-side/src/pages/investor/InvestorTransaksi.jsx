c431f3a8dc5d7d71b8361e2c89267ec2140d3233
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-09-02 10:45:26 +0700

    UPDATE : View Investor

--- client-side/src/pages/investor/InvestorTransaksi.jsx
+++ client-side/src/pages/investor/InvestorTransaksi.jsx
@@ -2,10 +2,8 @@ import { useState, useEffect } from "react";
 import SidebarInvestor from "../../components/common/SidebarInvestor";
 import InputSearch from "../../components/common/InputSearch";
 import Dropdown from "../../components/common/Dropdown";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
-import axios from "axios";
-import { jwtDecode } from "jwt-decode";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 import { getTransaksi } from "../../services/transaksi.service";
 import { useParams } from "react-router-dom";
@@ -13,10 +11,8 @@ import { formatRupiah } from "../../utils/formatRupiah";
 
 const InvestorTransaksi = () => {
     const [isHovered, setIsHovered] = useState(false);
-    const [username, setUsername] = useState('');
-    const [investorId, setInvestorId] = useState('');
-    const [token, setToken] = useState('');
-    const [expire, setExpire] = useState('');
+    const [token, setToken] = useState("");
+    const [expire, setExpire] = useState("");
     const [transaksi, setTransaksi] = useState([]);
     const [selectedValue, setSelectedValue] = useState("4");
     const [currentPage, setCurrentPage] = useState(1);
@@ -25,92 +21,19 @@ const InvestorTransaksi = () => {
     const { id } = useParams();
     const options = ["4", "8", "16", "32", "Semua"];
     const navigate = useNavigate();
-    const [investors, setInvestors] = useState([]);
     const [showModal, setShowModal] = useState(false); // State for controlling modal visibility
     const [selectedTransaction, setSelectedTransaction] = useState(null); // State for storing selected transaction
 
     useEffect(() => {
-        refreshToken();
-    }, []);
-
-    useEffect(() => {
-        if (token) {
-            getInvestors();
-        }
-    }, [token]);
-
-    useEffect(() => {
-        if (token) {
-            getTransaksi((data) => {
-                setTransaksi(data);
-                setLoading(false);
-            }, token);
-        }
-    }, [token]);
+        getTransaksi((data) => {
+            setTransaksi(data);
+            setLoading(false);
+        });
+    }, [navigate]);
 
     const totalInvestasi = transaksi.reduce((total, item) => {
         return total + item.total_investasi;
     }, 0);
-
-    const refreshToken = async () => {
-        try {
-            const response = await axios.post('http://localhost:3000/api/auth/investor/refresh-token', {}, { withCredentials: true });
-            setToken(response.data.accessToken);
-            const decoded = jwtDecode(response.data.accessToken);
-            setUsername(decoded.username);
-            setInvestorId(decoded.payload.investorId);
-            setExpire(decoded.exp);
-        } catch (error) {
-            if (error.response) {
-                navigate("/masuk");
-            }
-        }
-    };
-
-    const axiosJWT = axios.create();
-
-    axiosJWT.interceptors.request.use(
-        async (config) => {
-            const currentDate = new Date();
-            if (expire * 1000 < currentDate.getTime()) {
-                try {
-                    const response = await axios.post(
-                        'http://localhost:3000/api/auth/investor/refresh-token',
-                        {},
-                        { withCredentials: true }
-                    );
-                    const newAccessToken = response.data.accessToken;
-                    config.headers.Authorization = `Bearer ${newAccessToken}`;
-                    setToken(newAccessToken);
-                    const decoded = jwtDecode(newAccessToken);
-                    setUsername(decoded.username);
-                    setInvestorId(decoded.investorId);
-                    setExpire(decoded.exp);
-                } catch (error) {
-                    navigate("/investor");
-                }
-            } else {
-                config.headers.Authorization = `Bearer ${token}`;
-            }
-            return config;
-        },
-        (error) => {
-            return Promise.reject(error);
-        }
-    );
-
-    const getInvestors = async () => {
-        try {
-            const response = await axiosJWT.get('http://localhost:3000/api/investor', {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            setInvestors(response.data);
-        } catch (error) {
-            console.error("Error fetching investors:", error);
-        }
-    };
 
     const handleOptionSelect = (option) => {
         setSelectedValue(option);
@@ -125,18 +48,33 @@ const InvestorTransaksi = () => {
 
     return (
         <div className="bg-white w-dvw h-dvh overflow-y-auto py-5 pe-6">
-            <SidebarInvestor isHovered={isHovered} setIsHovered={setIsHovered} />
-            <div className={`px-8 pb-5 transition-all duration-300 ease-in-out ${isHovered ? "md:ml-60" : "md:ml-28"}`}>
+            <SidebarInvestor
+                isHovered={isHovered}
+                setIsHovered={setIsHovered}
+            />
+            <div
+                className={`px-8 pb-5 transition-all duration-300 ease-in-out ${
+                    isHovered ? "md:ml-60" : "md:ml-28"
+                }`}
+            >
                 <div className="flex flex-col">
-                    <h1 className="font-quicksand font-bold text-4xl mb-4">Transaksi</h1>
+                    <h1 className="font-quicksand font-bold text-4xl mb-4">
+                        Transaksi
+                    </h1>
                 </div>
                 <div className="w-full rounded-xl bg-[#F5F5F7] flex items-center mb-10 shadow-lg">
                     <div className="flex flex-col">
-                        <h1 className="text-[1.75rem] font-bold p-4 ml-8">Total Riwayat Investasi</h1>
+                        <h1 className="text-[1.75rem] font-bold p-4 ml-8">
+                            Total Riwayat Investasi
+                        </h1>
                         {loading ? (
-                            <p className="text-[2rem] font-medium p-4 ml-28">Loading...</p>
+                            <p className="text-[2rem] font-medium p-4 ml-28">
+                                Loading...
+                            </p>
                         ) : (
-                            <p className="text-[2rem] font-medium p-4 ml-28">{formatRupiah(totalInvestasi)}</p>
+                            <p className="text-[2rem] font-medium p-4 ml-28">
+                                {formatRupiah(totalInvestasi)}
+                            </p>
                         )}
                     </div>
                 </div>
@@ -155,33 +93,84 @@ const InvestorTransaksi = () => {
                         <table className="w-full text-base text-left text-gray-500">
                             <thead className="bg-white text-sm text-gray-700 uppercase border-b-2">
                                 <tr>
-                                    <th scope="col" className="px-6 py-3">No</th>
-                                    <th scope="col" className="px-6 py-3">Judul</th>
-                                    <th scope="col" className="px-6 py-3 text-center">Tanggal Transaksi</th>
-                                    <th scope="col" className="px-6 py-3 text-center">Investasi</th>
-                                    <th scope="col" className="px-6 py-3 text-center">Status</th>
-                                    <th scope="col" className="px-6 py-3 text-center">Pembayaran</th>
+                                    <th scope="col" className="px-6 py-3">
+                                        No
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Judul
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-center"
+                                    >
+                                        Tanggal Transaksi
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-center"
+                                    >
+                                        Investasi
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-center"
+                                    >
+                                        Status
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-center"
+                                    >
+                                        Pembayaran
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {transaksi.length > 0 ? (
                                     transaksi.map((transaction, index) => (
-                                        <tr key={transaction.id} className="bg-white border-b hover:bg-gray-50">
-                                            <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
+                                        <tr
+                                            key={transaction.id}
+                                            className="bg-white border-b hover:bg-gray-50"
+                                        >
+                                            <th
+                                                scope="row"
+                                                className="px-6 py-4 font-medium whitespace-nowrap"
+                                            >
                                                 {index + 1}
                                             </th>
-                                            <td className="px-6 py-4">{transaction.investasi.judul}</td>
-                                            <td className="px-6 py-4 text-center">{transaction.tanggal_transaksi}</td>
-                                            <td className="px-6 py-4 text-center">{formatRupiah(transaction.total_investasi)}</td>
-                                            <td className="px-6 py-4 text-center">{transaction.status}</td>
+                                            <td className="px-6 py-4">
+                                                {transaction.investasi.judul}
+                                            </td>
                                             <td className="px-6 py-4 text-center">
-                                                <FontAwesomeIcon icon={faEye} className="cursor-pointer" onClick={() => handleViewDetails(transaction)} />
+                                                {transaction.tanggal_transaksi}
+                                            </td>
+                                            <td className="px-6 py-4 text-center">
+                                                {formatRupiah(
+                                                    transaction.total_investasi
+                                                )}
+                                            </td>
+                                            <td className="px-6 py-4 text-center">
+                                                {transaction.status}
+                                            </td>
+                                            <td className="px-6 py-4 text-center">
+                                                <FontAwesomeIcon
+                                                    icon={faEye}
+                                                    className="cursor-pointer"
+                                                    onClick={() =>
+                                                        handleViewDetails(
+                                                            transaction
+                                                        )
+                                                    }
+                                                />
                                             </td>
                                         </tr>
                                     ))
                                 ) : (
                                     <tr>
-                                        <td colSpan="6" className="px-6 py-4 text-center">
+                                        <td
+                                            colSpan="6"
+                                            className="px-6 py-4 text-center"
+                                        >
                                             Tidak ada transaksi ditemukan
                                         </td>
                                     </tr>
@@ -191,42 +180,56 @@ const InvestorTransaksi = () => {
                     </div>
                 </div>
                 {showModal && (
-                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="relative bg-white p-6 rounded-lg shadow-lg w-2/3">
-                        {/* Tombol silang di pojok kanan atas */}
-                        <button
-                            className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
-                            onClick={() => setShowModal(false)}
-                            style={{ fontSize: '24px', padding: '0.5rem' }}
-                        >
-                            &times;
-                        </button>
+                    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                        <div className="relative bg-white p-6 rounded-lg shadow-lg w-2/3">
+                            {/* Tombol silang di pojok kanan atas */}
+                            <button
+                                className="absolute top-4 right-4 text-gray-500 hover:text-gray-800"
+                                onClick={() => setShowModal(false)}
+                                style={{ fontSize: "24px", padding: "0.5rem" }}
+                            >
+                                &times;
+                            </button>
 
-                        <h2 className="text-2xl font-bold mb-4">Detail Investasi</h2>
-                        {selectedTransaction ? (
-                            <div className="space-y-4">
-                                <div>
-                                    <p className="font-semibold">ID Transaksi:</p>
-                                    <p>xxxxxxxx</p>
+                            <h2 className="text-2xl font-bold mb-4">
+                                Detail Investasi
+                            </h2>
+                            {selectedTransaction ? (
+                                <div className="space-y-4">
+                                    <div>
+                                        <p className="font-semibold">
+                                            ID Transaksi:
+                                        </p>
+                                        <p>xxxxxxxx</p>
+                                    </div>
+                                    <div>
+                                        <p className="font-semibold">
+                                            Metode Pembayaran:
+                                        </p>
+                                        <p>xxx</p>
+                                    </div>
+                                    <div>
+                                        <p className="font-semibold">
+                                            Nomor Rekening:
+                                        </p>
+                                        <p>xxxxxxxxxxxxxxxxxx</p>
+                                    </div>
+                                    <div>
+                                        <p className="font-semibold">
+                                            Tanggal Transaksi:
+                                        </p>
+                                        <p>
+                                            {
+                                                selectedTransaction.tanggal_transaksi
+                                            }
+                                        </p>
+                                    </div>
                                 </div>
-                                <div>
-                                    <p className="font-semibold">Metode Pembayaran:</p>
-                                    <p>xxx</p>
-                                </div>
-                                <div>
-                                    <p className="font-semibold">Nomor Rekening:</p>
-                                    <p>xxxxxxxxxxxxxxxxxx</p>
-                                </div>
-                                <div>
-                                    <p className="font-semibold">Tanggal Transaksi:</p>
-                                    <p>{selectedTransaction.tanggal_transaksi}</p>
-                                </div>
-                            </div>
-                        ) : (
-                            <p>Loading...</p>
-                        )}
+                            ) : (
+                                <p>Loading...</p>
+                            )}
+                        </div>
                     </div>
-                </div>
                 )}
             </div>
         </div>
--- client-side/src/services/transaksi.service.js
+++ client-side/src/services/transaksi.service.js
@@ -1,18 +1,15 @@
-import { axiosInstance } from "../lib/axios";
+// import { axiosInstance } from "../lib/axios";
+import { axiosInstance } from "../hooks/useAxiosConfig";
 
-export const getTransaksi = (callback, token) => {
-  axiosInstance
-    .get('/investor/transaksi/all', {
-      headers: {
-        Authorization: `Bearer ${token}`, // Sertakan token di sini
-      },
-    })
-    .then((res) => {
-      console.log("API response:", res.data); // Check the data structure
-      console.log("API response:", res.data.data); // Check the data structure
-      callback(res.data.data); // Make sure the data structure is correct
-    })
-    .catch((err) => {
-      console.log("API Error:", err);
-    });
+export const getTransaksi = (callback) => {
+    axiosInstance
+        .get("/investor/me/transaksi")
+        .then((res) => {
+            console.log("API response:", res.data); // Check the data structure
+            console.log("API response:", res.data.data); // Check the data structure
+            callback(res.data.data); // Make sure the data structure is correct
+        })
+        .catch((err) => {
+            console.log("API Error:", err);
+        });
 };
