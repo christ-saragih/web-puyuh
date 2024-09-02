e37e81086b5642d33f6ad80c16a4ee1606f59189
Jonathan <nath.joe34@gmail.com>
2024-09-02 16:45:53 +0700

    UPDATE get total investasi & batch investasi data to investor dashboard

---
+++ client-side/src/components/investor/BatchListInvestor.jsx
@@ -0,0 +1,41 @@
+import BatchItemInvestor from "./BatchItemInvestor";
+
+const BatchListInvestor = ({ batchs }) => {
+  return (
+    <div className="overflow-x-auto w-full">
+      <div className="flex gap-x-6">
+        {Array.isArray(batchs) && batchs.length > 0 ? (
+          batchs.map((batch) => (
+            <div key={batch.id} className="flex-shrink-0 w-[20rem]">
+              <BatchItemInvestor
+                id={batch.id}
+                judul={batch.judul}
+                deskripsi={batch.deskripsi}
+                gambar={batch.gambar}
+                alamat={batch.alamat}
+                map={batch.url_map}
+                slug={batch.slug}
+                penerbit={batch.penerbit}
+                dana={batch.penggunaan_dana}
+                bagi={batch.bagi_hasil}
+                minimum={batch.minimum_investasi}
+                maksimum={batch.maksimum_investasi}
+                total={batch.total_pendanaan}
+                target={batch.target_pendanaan}
+                tenor={batch.tenor}
+                pembayaran={batch.pembayaran_bagi_hasil}
+                pembukaan={batch.tanggal_pembukaan_penawaran}
+                penutupan={batch.tanggal_berakhir_penawaran}
+                status={batch.status}
+              />
+            </div>
+          ))
+        ) : (
+          <p>No batches available</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BatchListInvestor;
