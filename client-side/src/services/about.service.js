0f121f557a0c86e956b9b1b5b73aead549b2cd8a
Jonathan <nath.joe34@gmail.com>
2024-09-05 05:12:01 +0700

    UPDATE fetch data tentangkami in service

--- client-side/src/services/about.service.js
+++ client-side/src/services/about.service.js
@@ -4,7 +4,11 @@ export const getAbouts = (callback) => {
   axios
     .get("http://localhost:3000/api/tentang-kami")
     .then((res) => {
-      callback(res.data);
+      callback(res.data.data);
+      console.log("DATA TENTANG KAMI");
+      console.log(res.data.data);
+      
+      
     })
     .catch((err) => {
       console.log(err);
@@ -15,7 +19,7 @@ export const getAboutSejarahs = (callback) => {
   axios
     .get("http://localhost:3000/api/sejarah")
     .then((res) => {
-      callback(res.data);
+      callback(res.data.data);
     })
     .catch((err) => {
       console.log(err);
