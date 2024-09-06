10f4e6fefbaa11828de10865f16d501d8485077c
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-09-06 15:30:33 +0700

    UPDATE : hapus fungsi delete, menambahkan authenticateToken Upsert, dan Memperbaiki Validasi di Tentang Kami

--- server-side/src/routes/tentangKamiRoutes.js
+++ server-side/src/routes/tentangKamiRoutes.js
@@ -3,28 +3,29 @@ const express = require("express");
 const router = express.Router();
 const tentangKamiContoller = require("../controllers/tentangKamiContoller");
 const validate = require("../middleware/validationMiddleware");
-const {
-    createSchema,
-    updateSchema,
-} = require("../validators/tantangKamiValidation");
+const { upsertSchema } = require("../validators/tantangKamiValidation");
 
 const upload = require("../middleware/uploadFileMiddleware");
 const {
     validateUploadFile,
 } = require("../middleware/validationUploadFileMiddleware");
 
+// Auth
+const { authenticateToken } = require("../middleware/authenticateToken");
+
 router.post(
     "/",
+    authenticateToken("admin"),
     upload.single("image_background"),
     validateUploadFile({
         fieldName: "image_background",
         required: false,
     }),
-    validate(createSchema),
+    validate(upsertSchema),
     tentangKamiContoller.upsert
 );
 router.get("/", tentangKamiContoller.findData);
-router.delete("/:id", tentangKamiContoller.delete);
+// router.delete("/:id", tentangKamiContoller.delete);
 router.get("/image/:gambar", tentangKamiContoller.getImageByName);
 
 module.exports = router;
