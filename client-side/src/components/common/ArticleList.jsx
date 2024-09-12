874f05caa8a3ce7f16856e9dee1be7e37d6429b4
christ-saragih <bennefit.19@gmail.com>
2024-09-12 11:32:07 +0700

    CREATED: Fitur pencarian data artikel di guest dan tampilan saat artikel tidak ada/tidak ditemukan

--- client-side/src/components/common/ArticleList.jsx
+++ client-side/src/components/common/ArticleList.jsx
@@ -1,19 +1,41 @@
 import ArticleItem from "./ArticleItem";
+import { LuBadgeInfo } from "react-icons/lu";
 
 const ArticleList = (props) => {
   const { articles, role, openModal } = props;
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-10 gap-y-8 xl:gap-x-14 xl:gap-y-10">
-      {articles.map((article) => (
-        <ArticleItem
-          key={article.id}
-          {...article}
-          role={role}
-          openModal={openModal}
-        />
-      ))}
-    </div>
+    <>
+      {Array.isArray(articles) && articles.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-10 gap-y-8 xl:gap-x-14 xl:gap-y-10">
+          {articles.map((article) => (
+            <ArticleItem
+              key={article.id}
+              {...article}
+              role={role}
+              openModal={openModal}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center ">
+          <div
+            className="flex items-center w-full max-w-xl px-4 py-3 mb-4  text-[#5766CE] rounded-2xl bg-[#EEEFFA] border border-[#ccd1f0] shadow"
+            role="alert"
+          >
+            <div className="bg-[#5766CE] rounded-xl w-9 h-9 p-[6px] me-2 ">
+              <LuBadgeInfo className="w-full h-full text-white" />
+            </div>
+
+            <span className="sr-only">Info</span>
+            <div>
+              <span className="font-medium">Pemberitahuan!</span> Tidak ada
+              artikel yang tersedia.
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- client-side/src/layouts/GuestLayouts/index.jsx
+++ client-side/src/layouts/GuestLayouts/index.jsx
@@ -8,95 +8,80 @@ import { guestMenus } from "./MenuList";
 import { AuthProvider } from "../../contexts/AuthProvider";
 
 const GuestLayouts = (props) => {
-    const { children } = props;
-    return (
-        <div>
-            <AuthProvider>
-                <Navbar />
-            </AuthProvider>
-            {children}
+  const { children } = props;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <AuthProvider>
+        <Navbar />
+      </AuthProvider>
+      {children}
 
-            <ScrollToTopButton />
-        </div>
-    );
+      <ScrollToTopButton />
+    </div>
+  );
 };
 
 const Footer = () => {
-    const [socialMedias, setSocialMedias] = useState([]);
+  const [socialMedias, setSocialMedias] = useState([]);
 
-    useEffect(() => {
-        getSocialMedia((data) => {
-            setSocialMedias(data);
-        });
-    }, []);
+  useEffect(() => {
+    getSocialMedia((data) => {
+      setSocialMedias(data);
+    });
+  }, []);
 
-    return (
-        <footer className="bg-[#4B241A] ">
-            <div className="mx-auto w-full max-w-screen-xl px-4 pt-6 pb-4 lg:pt-8 lg:pb-5 ">
-                <div className="md:flex md:justify-between">
-                    <div className="mb-6 md:mb-0">
-                        <Link to={"/"} className="flex items-center">
-                            <img
-                                src={LogoPutih}
-                                alt="Logo Putih"
-                                className="h-10 sm:h-12 lg:h-14 me-3"
-                            />
-                            <span className="self-center text-gray-100 text-xl lg:text-2xl font-semibold whitespace-nowrap">
-                                PT Sukaharja Quail Indonesia
-                            </span>
-                        </Link>
-                    </div>
+  return (
+    <footer className="bg-[#4B241A] ">
+      <div className="mx-auto w-full max-w-screen-xl px-4 pt-6 pb-4 lg:pt-8 lg:pb-5 ">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <Link to={"/"} className="flex items-center">
+              <img
+                src={LogoPutih}
+                alt="Logo Putih"
+                className="h-10 sm:h-12 lg:h-14 me-3"
+              />
+              <span className="self-center text-gray-100 text-xl lg:text-2xl font-semibold whitespace-nowrap">
+                PT Sukaharja Quail Indonesia
+              </span>
+            </Link>
+          </div>
 
-                    <ul className="flex items-center space-x-4 md:space-x-6 font-medium text-gray-100 sm:mb-0 overflow-auto">
-                        {guestMenus.map((guestMenus, index) => (
-                            <li
-                                key={`${index}-${guestMenus.text}`}
-                                className="min-w-fit"
-                            >
-                                <Link
-                                    to={guestMenus.link}
-                                    className="hover:underline"
-                                >
-                                    {guestMenus.text}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <hr className="mt-6 border-[#EFEFEF] sm:mx-auto lg:mt-8 mb-4" />
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-[#EFEFEF] text-sm sm:text-center">
-                        © 2024{" "}
-                        <Link
-                            to={"/"}
-                            className="font-semibold hover:underline"
-                        >
-                            PT Sukaharja Quail Indonesia
-                        </Link>
-                        . All Rights Reserved.
-                    </span>
-                    <div className="flex mt-4 sm:justify-center sm:mt-0 space-x-2 md:space-x-4">
-                        {socialMedias.map((socialMedia) => (
-                            <Link
-                                key={socialMedia.id}
-                                to={socialMedia.url}
-                                target="_blank"
-                            >
-                                <img
-                                    src={`http://localhost:3000/api/sosial-media/image/${socialMedia.icon}`}
-                                    alt={socialMedia.nama}
-                                    className="w-10 h-10 rounded-full"
-                                />
-                                <span className="sr-only">
-                                    {socialMedia.nama}
-                                </span>
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+          <ul className="flex items-center space-x-4 md:space-x-6 font-medium text-gray-100 sm:mb-0 overflow-auto">
+            {guestMenus.map((guestMenus, index) => (
+              <li key={`${index}-${guestMenus.text}`} className="min-w-fit">
+                <Link to={guestMenus.link} className="hover:underline">
+                  {guestMenus.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <hr className="mt-6 border-[#EFEFEF] sm:mx-auto lg:mt-8 mb-4" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-[#EFEFEF] text-sm sm:text-center">
+            © 2024{" "}
+            <Link to={"/"} className="font-semibold hover:underline">
+              PT Sukaharja Quail Indonesia
+            </Link>
+            . All Rights Reserved.
+          </span>
+          <div className="flex mt-4 sm:justify-center sm:mt-0 space-x-2 md:space-x-4">
+            {socialMedias.map((socialMedia) => (
+              <Link key={socialMedia.id} to={socialMedia.url} target="_blank">
+                <img
+                  src={`http://localhost:3000/api/sosial-media/image/${socialMedia.icon}`}
+                  alt={socialMedia.nama}
+                  className="w-10 h-10 rounded-full"
+                />
+                <span className="sr-only">{socialMedia.nama}</span>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 GuestLayouts.Footer = Footer;
