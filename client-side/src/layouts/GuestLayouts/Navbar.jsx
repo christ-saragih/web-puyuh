d79ca22955bf5368f510a8ca02c8381484b1fec6
Jonathan <nath.joe34@gmail.com>
2024-09-18 16:12:30 +0700

    UPDATE image user and dropdown on navbar and investor dashboard

--- client-side/src/layouts/GuestLayouts/Navbar.jsx
+++ client-side/src/layouts/GuestLayouts/Navbar.jsx
@@ -44,48 +44,52 @@ const Navbar = () => {
     }
   }, [user]);
 
+  const UserProfileImage = () => (
+    <>
+      {user.role === "admin" && admin && admin.adminBiodata.foto_profil ? (
+        <img
+          src={admin.adminBiodata.foto_profil}
+          alt={admin.adminBiodata.foto_profil}
+          className="w-10 h-10 rounded-full"
+        />
+      ) : user.role === "investor" && investor && investor.investorBiodata.foto_profil ? (
+        <img
+          src={`http://localhost:3000/api/biodata-investor/images/${investor.investorBiodata.foto_profil}`}
+          alt={investor.investorBiodata.foto_profil}
+          className="w-10 h-10 rounded-full"
+        />
+      ) : (
+        <img
+          src={Admin}
+          alt="Default Profile"
+          className="w-10 h-10 rounded-full"
+        />
+      )}
+    </>
+  );
+
   return (
     <nav className="bg-white w-full z-20 top-0 start-0 shadow-md shadow-[#4B241A] rounded-b-2xl">
       <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto lg:px-14 py-2">
-        <NavLink
-          to="/"
-          className="flex items-center space-x-3 ms-6 md:ms-10 lg:ms-0 py-2"
-        >
-          <img src={Logo} className="h-10 sm:h-12 lg:h-14" alt="Logo" />
-          <span className="hidden xl:block self-center text-2xl font-semibold whitespace-nowrap">
-            PT Sukaharja Quail Indonesia
-          </span>
-        </NavLink>
+        {/* Logo and Brand Name */}
+        <div className="flex items-center space-x-3 ms-6 md:ms-10 lg:ms-0 py-2">
+          <NavLink to="/" className="flex items-center space-x-3">
+            <img src={Logo} className="h-10 sm:h-12 lg:h-14" alt="Logo" />
+            <span className="hidden xl:block self-center text-2xl font-semibold whitespace-nowrap">
+              PT Sukaharja Quail Indonesia
+            </span>
+          </NavLink>
+        </div>
 
-        <div className="hidden lg:flex lg:items-center lg:order-2 gap-2 space-x-3 lg:space-x-0 ">
-          {user ? (
-            <>
-              {/* Tampilan saat user sudah login */}
-              {user.role === "admin" && admin && admin.adminBiodata.foto_profil ? (
-                <img
-                  src={admin.adminBiodata.foto_profil}
-                  alt={admin.adminBiodata.foto_profil}
-                  className="w-10 h-10 rounded-full"
-                />
-              ) : user.role === "investor" && investor &&
-                investor.investorBiodata.foto_profil ? (
-                <img
-                  src={`http://localhost:3000/api/biodata-investor/images/${investor.investorBiodata.foto_profil}`}
-                  alt={investor.investorBiodata.foto_profil}
-                  className="w-10 h-10 rounded-full"
-                />
-              ) : (
-                <img
-                  src={Admin}
-                  alt="Default Profile"
-                  className="w-10 h-10 rounded-full"
-                />
-              )}
-
+        {/* User Profile and Menu Toggle for Mobile */}
+        <div className="flex items-center lg:order-2">
+          {user && (
+            <div className="flex items-center space-x-2 mr-4 lg:mr-0">
+              <UserProfileImage />
               <Dropdown
                 label=""
                 dismissOnClick={false}
-                placement="bottom-start"
+                placement="bottom-end"
                 renderTrigger={() => (
                   <span className="cursor-pointer">
                     <LuChevronDown className="w-5 h-5" />
@@ -109,10 +113,48 @@ const Navbar = () => {
                   Keluar
                 </Dropdown.Item>
               </Dropdown>
-            </>
-          ) : (
-            <>
-              {/* Tampilan saat user belum login */}
+            </div>
+          )}
+          <button
+            onClick={handleToggle}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-sticky"
+            aria-expanded={isOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Menu Items and Login/Register Buttons */}
+        <div
+          className={`items-center justify-between w-full lg:flex lg:w-auto lg:order-1 ${
+            isOpen ? "block" : "hidden"
+          }`}
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 lg:p-0 lg:space-x-2 lg:flex-row lg:mt-0 lg:border-0">
+            {guestMenus.map((guestMenu, index) => (
+              <MenuItem key={`${index}-${guestMenu.text}`} {...guestMenu} />
+            ))}
+          </ul>
+          {!user && (
+            <div className="flex justify-center gap-3 mt-4 py-4 bg-gray-50 lg:hidden">
               <Link to="/masuk">
                 <button
                   type="button"
@@ -129,42 +171,13 @@ const Navbar = () => {
                   Daftar
                 </button>
               </Link>
-            </>
+            </div>
           )}
         </div>
 
-        <button
-          onClick={handleToggle}
-          type="button"
-          className="inline-flex items-center p-2 me-4 md:me-8 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-          aria-controls="navbar-sticky"
-          aria-expanded={isOpen}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-
-        <div
-          className={`items-center justify-between w-full lg:flex lg:w-auto lg:order-1 ${
-            isOpen ? "block" : "hidden"
-          }`}
-          id="navbar-sticky"
-        >
-          <div className="flex justify-center gap-3 mt-4 py-4 bg-gray-50 lg:hidden">
+        {/* Login/Register Buttons for Desktop */}
+        {!user && (
+          <div className="hidden lg:flex lg:items-center lg:order-2 gap-2 space-x-3 lg:space-x-0">
             <Link to="/masuk">
               <button
                 type="button"
@@ -182,12 +195,7 @@ const Navbar = () => {
               </button>
             </Link>
           </div>
-          <ul className="flex flex-col p-4 lg:p-0 lg:space-x-2 lg:flex-row lg:mt-0 lg:border-0">
-            {guestMenus.map((guestMenu, index) => (
-              <MenuItem key={`${index}-${guestMenu.text}`} {...guestMenu} />
-            ))}
-          </ul>
-        </div>
+        )}
       </div>
     </nav>
   );
