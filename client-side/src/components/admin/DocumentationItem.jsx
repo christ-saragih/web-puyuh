ca26cff693c4e132762844c675c7a12aed08062a
christ-saragih <bennefit.19@gmail.com>
2024-09-20 14:59:58 +0700

    FIXING: Konsumsi API dokumentasi. CREATED: Fitur pencarian dokumentasi & Tampilan dokumentasi saat kosong

--- client-side/src/components/admin/DocumentationItem.jsx
+++ client-side/src/components/admin/DocumentationItem.jsx
@@ -1,11 +1,9 @@
-import { PiNotePencilBold, PiTrashBold } from "react-icons/pi";
 import ActionButton from "../common/ActionButton";
+import { PiNotePencilBold, PiTrashBold } from "react-icons/pi";
 
 const DocumentationItem = (props) => {
   const { id, nama, image, openModal } = props;
 
-  console.log(image);
-  
   return (
     <div className="bg-white flex gap-2 py-3 px-6 rounded-2xl shadow-lg flex-wrap justify-center">
       <h3 className="font-medium">{nama}</h3>
@@ -15,21 +13,13 @@ const DocumentationItem = (props) => {
           alt={nama}
           className="w-full h-full object-top object-cover rounded-xl"
         />
-        {/* GET image dari seeder */}
-        {/* <img
-          src={image}
-          alt={nama}
-          className="w-full h-full object-cover rounded-xl"
-        /> */}
       </div>
       <div className="space-x-2 mb-2">
         <ActionButton
           icon={PiNotePencilBold}
           className={"text-yellow-600"}
           tooltip={"Ubah"}
-            onClick={() =>
-              openModal("update_documentation", { id, nama, image })
-            }
+          onClick={() => openModal("update_documentation", { id, nama, image })}
         />
         <ActionButton
           icon={PiTrashBold}
