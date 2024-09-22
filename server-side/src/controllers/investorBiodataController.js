64e107873b4f24f7b5b17663576122a536afd0ef
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-09-22 19:23:22 +0700

    UPDATE : Menghapus Fungsi Create, Update,  Delete, dan Find All di Investor Biodata

--- server-side/src/controllers/investorBiodataController.js
+++ server-side/src/controllers/investorBiodataController.js
@@ -9,211 +9,7 @@ const ensureDir = (dir) => {
     }
 };
 
-// Create
-exports.create = async (req, res) => {
-    try {
-        const { nama_lengkap, jk, tempat_lahir, tanggal_lahir, no_hp } =
-            req.body;
-
-        const foto_profil = req.file ? req.file.buffer : null;
-
-        if (
-            foto_profil &&
-            nama_lengkap &&
-            jk &&
-            tempat_lahir &&
-            tanggal_lahir &&
-            no_hp
-        ) {
-            const dir = "public/images/investors/profile";
-            ensureDir(dir);
-            nama_foto = `${Date.now()}-${req.file.originalname}`;
-            fs.writeFileSync(path.join(dir, nama_foto), foto_profil);
-        }
-
-        const investorId = req.investor.id;
-        const investorBiodata = await InvestorBiodata.create({
-            investorId: investorId,
-            nama_lengkap,
-            jk,
-            tempat_lahir,
-            tanggal_lahir,
-            no_hp,
-            foto_profil: nama_foto,
-        });
-
-        res.status(201).json({
-            message: "Biodata Berhasil Ditambahkan!",
-            data: investorBiodata,
-        });
-    } catch (error) {
-        if (error.name === "SequelizeValidationError") {
-            const messages = error.errors.map((err) => err.message);
-            res.status(400).json({
-                message: "Validation error",
-                errors: messages,
-            });
-        } else {
-            res.status(500).json({
-                message: "Internal server error",
-                error: error.message,
-            });
-        }
-    }
-};
-
-// Read All
-exports.findAll = async (req, res) => {
-    try {
-        const investorBiodata = await InvestorBiodata.findAll();
-        res.status(200).json({
-            message: "Biodata Investor berhasil diambil!",
-            data: investorBiodata,
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Internal server error",
-            error: error.message,
-        });
-    }
-};
-
-// Read One
-exports.findOne = async (req, res) => {
-    try {
-        const investorBiodata = await InvestorBiodata.findByPk(req.params.id);
-        if (!investorBiodata) {
-            return res
-                .status(404)
-                .json({ message: "Biodata Investor tidak ada!" });
-        }
-        res.status(200).json({
-            message: "Biodata Investor berhasil diambil",
-            data: investorBiodata,
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Internal server error",
-            error: error.message,
-        });
-    }
-};
-
-// Update
-exports.update = async (req, res) => {
-    try {
-        const { nama_lengkap, jk, tempat_lahir, tanggal_lahir, no_hp } =
-            req.body;
-
-        const foto_profil = req.file ? req.file.buffer : null;
-
-        const investorBiodata = await InvestorBiodata.findByPk(req.params.id);
-
-        if (!investorBiodata) {
-            return res
-                .status(404)
-                .json({ message: "Biodata investor tidak ada!" });
-        }
-
-        let nama_foto = investorBiodata.foto_profil;
-        if (foto_profil) {
-            const dir = "public/images/investors/profile";
-            ensureDir(dir);
-            nama_foto = `${Date.now()}-${req.file.originalname}`;
-            fs.writeFileSync(path.join(dir, nama_foto), foto_profil);
-
-            if (investorBiodata.foto_profil) {
-                const oldImagePath = path.join(
-                    dir,
-                    investorBiodata.foto_profil
-                );
-                if (fs.existsSync(oldImagePath)) {
-                    fs.unlinkSync(oldImagePath);
-                }
-            }
-        }
-
-        await investorBiodata.update({
-            nama_lengkap,
-            jk,
-            tempat_lahir,
-            tanggal_lahir,
-            no_hp,
-            foto_profil: nama_foto,
-        });
-
-        res.status(200).json({
-            message: "Biodata investor berhasil diperbaharui",
-            data: investorBiodata,
-        });
-    } catch (error) {
-        if (error.name === "SequelizeValidationError") {
-            const messages = error.errors.map((err) => err.message);
-            res.status(400).json({
-                message: "Validation error",
-                errors: messages,
-            });
-        } else {
-            res.status(500).json({
-                message: "Internal server error",
-                error: error.message,
-            });
-        }
-    }
-};
-
-// Delete
-exports.delete = async (req, res) => {
-    try {
-        const investorBiodata = await InvestorBiodata.findByPk(req.params.id);
-        if (!investorBiodata) {
-            return res
-                .status(404)
-                .json({ message: "Biodata Investor tidak ada!" });
-        }
-
-        // Delete image file
-        if (investorBiodata.foto_profil) {
-            const imagePath = path.resolve(
-                `public/images/investors/profile/${investorBiodata.foto_profil}`
-            );
-            if (fs.existsSync(imagePath)) {
-                fs.unlink(imagePath, (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-            }
-        }
-
-        await investorBiodata.destroy();
-        res.status(200).json({
-            message: "Biodata Investor berhasil dihapus!",
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Internal server error",
-            error: error.message,
-        });
-    }
-};
-
-// Get Image by Name
-exports.getImageByName = (req, res) => {
-    const { gambar } = req.params;
-    const dir = "public/images/investors/profile";
-    const imagePath = path.join(dir, gambar);
-
-    if (fs.existsSync(imagePath)) {
-        res.sendFile(path.resolve(imagePath));
-    } else {
-        res.status(404).json({
-            message: "Gambar tidak ditemukan",
-        });
-    }
-};
-
-// upsert
+// Create atau Update Investor Biodata
 exports.upsert = async (req, res) => {
     try {
         const {
@@ -319,5 +115,41 @@ exports.upsert = async (req, res) => {
                 error: error.message,
             });
         }
+    }
+};
+
+// Read One
+exports.findOne = async (req, res) => {
+    try {
+        const investorBiodata = await InvestorBiodata.findByPk(req.params.id);
+        if (!investorBiodata) {
+            return res
+                .status(404)
+                .json({ message: "Biodata Investor tidak ada!" });
+        }
+        res.status(200).json({
+            message: "Biodata Investor berhasil diambil",
+            data: investorBiodata,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message,
+        });
+    }
+};
+
+// Get Image by Name
+exports.getImageByName = (req, res) => {
+    const { gambar } = req.params;
+    const dir = "public/images/investors/profile";
+    const imagePath = path.join(dir, gambar);
+
+    if (fs.existsSync(imagePath)) {
+        res.sendFile(path.resolve(imagePath));
+    } else {
+        res.status(404).json({
+            message: "Gambar tidak ditemukan",
+        });
     }
 };
