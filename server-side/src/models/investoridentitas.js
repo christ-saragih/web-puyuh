2b2ba035c7e6b52846b6e89eb61c5eaa598af2e3
iqbalfadhila <iqbalfadhila8902@gmail.com>
2024-10-23 13:41:09 +0700

    UPDATE : Nama table model

--- server-side/src/models/investoridentitas.js
+++ server-side/src/models/investoridentitas.js
@@ -47,7 +47,6 @@ module.exports = (sequelize, DataTypes) => {
         {
             sequelize,
             modelName: "InvestorIdentitas",
-            tableName: "investoridentitas",
         }
     );
     return InvestorIdentitas;
