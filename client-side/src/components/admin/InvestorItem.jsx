0aab2b9c70b06a41aba2eca5e4c5011de52a25eb
christ-saragih <bennefit.19@gmail.com>
2024-10-24 15:43:57 +0700

    CREATED: Fitur memverifikasi dan membatalkan verifikasi investor

--- client-side/src/components/admin/InvestorItem.jsx
+++ client-side/src/components/admin/InvestorItem.jsx
@@ -1,45 +1,83 @@
-import { PiUserBold, PiUsersThreeBold } from "react-icons/pi";
+import { Dropdown, Tooltip } from "flowbite-react";
+import ToggleButton from "../common/ToggleButton";
+import {
+  PiDotsThreeOutlineBold,
+  PiFolderUser,
+  PiMoneyWavy,
+  PiUserBold,
+  PiUsersThreeBold,
+} from "react-icons/pi";
 
 const InvestorItem = (props) => {
-  const { id, username, kategori_investor, investorBiodata, openModal } = props;
+  const {
+    id,
+    username,
+    kategori_investor,
+    investorBiodata,
+    openModal,
+    isVerifiedProfile,
+    handleToggleStatus,
+  } = props;
 
   return (
-    <div className="bg-white flex flex-col items-center xl:flex-row px-5 py-6 gap-4 rounded-2xl shadow-lg">
-      {investorBiodata?.foto_profil ? (
-        <img
-          src={investorBiodata.foto_profil}
-          alt={investorBiodata.foto_profil}
-          className="w-20 h-20 rounded-full "
+    <div className="relative bg-white flex flex-col items-center px-5 py-6 rounded-2xl shadow-lg card-investor">
+      <Tooltip
+        content={
+          isVerifiedProfile ? "Batalkan Verifikasi" : "Verifikasi Investor"
+        }
+        placement="top"
+      >
+        <ToggleButton
+          isChecked={isVerifiedProfile}
+          handleToggleStatus={() => handleToggleStatus(id, isVerifiedProfile)}
         />
+      </Tooltip>
+
+      {investorBiodata?.foto_profil ? (
+        <div className="w-24 h-24 rounded-full overflow-hidden mb-2">
+          <img
+            src={investorBiodata.foto_profil}
+            alt={investorBiodata.foto_profil}
+            className="w-full h-full object-cover"
+          />
+        </div>
       ) : (
-        <div>
-          <div className="w-20 h-20 rounded-full overflow-hidden bg-gray-200 p-2">
-            {kategori_investor === "organisasi" ? (
-              <PiUsersThreeBold className="w-full h-full" />
-            ) : (
-              <PiUserBold className="w-full h-full" />
-            )}
-          </div>
+        <div className="w-20 h-20 rounded-full overflow-hidden bg-gray-200 p-2">
+          {kategori_investor === "organisasi" ? (
+            <PiUsersThreeBold className="w-full h-full" />
+          ) : (
+            <PiUserBold className="w-full h-full" />
+          )}
         </div>
       )}
 
-      <div className="flex flex-col items-center xl:inline truncate">
-        <h4 className="font-semibold truncate">
-          {username}
-        </h4>
-        <p className="text-gray-600 mb-2">{kategori_investor}</p>
-        <button
-          className="font-medium text-start me-2 w-fit text-blue-700 hover:text-blue-800 hover:underline"
-          onClick={() => openModal("detail_investor", { id })}
+      <h4 className="font-semibold truncate">{username}</h4>
+      <p className="text-gray-600 mb-2">{kategori_investor}</p>
+
+      <div className="absolute top-5 right-5 cursor-pointer duration-300 hover:shadow-lg rounded-full">
+        <Dropdown
+          label=""
+          dismissOnClick={false}
+          renderTrigger={() => (
+            <div className="border border-gray-300 rounded-full p-[6px]">
+              <PiDotsThreeOutlineBold className="text-gray-400" />
+            </div>
+          )}
+          placement="right-end"
         >
-          Profil
-        </button>
-        <button
-          className="font-medium text-start w-fit text-blue-700 hover:text-blue-800 hover:underline"
-          onClick={() => openModal("view_transactions", { id })}
-        >
-          Riwayat
-        </button>
+          <Dropdown.Item
+            icon={PiFolderUser}
+            onClick={() => openModal("detail_investor", id)}
+          >
+            Profil
+          </Dropdown.Item>
+          <Dropdown.Item
+            icon={PiMoneyWavy}
+            onClick={() => openModal("view_transactions", id)}
+          >
+            Transaksi
+          </Dropdown.Item>
+        </Dropdown>
       </div>
     </div>
   );
